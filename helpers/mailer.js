c60ddb1e24d40ce82c5798d360769b01909506ef
megagonn <yusuffrasheed.yr@gmail.com>
2024-09-09 14:28:01 +0100

    commit

--- helpers/mailer.js
+++ helpers/mailer.js
@@ -7,7 +7,7 @@ const mg = mailgun.client({ username: 'meg', key: process.env.MAILGUN_API_KEY })
 const sendMail = (subject, to, content) => {
 
     mg.messages.create('sandbox3d50866775f34b129cc28922b27d0131.mailgun.org', {
-        from: "PARTYXPENDER",
+        from: "PARTYXPENDER <mgt@partyxpender.com>",
         to: [to],
         subject: subject,
         text: content,
