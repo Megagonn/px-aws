5dbe59618ff49abbb6fd93387a98e131d82b10ad
megagonn <yusuffrasheed.yr@gmail.com>
2024-09-14 11:44:43 +0100

    commit

--- index.js
+++ index.js
@@ -172,6 +172,7 @@ app.get("/privacy", (req, res)=>{
     res.sendFile(__dirname +'/static/privacy.html');
 });
 app.get('/support', (req, res) => res.sendFile(__dirname +'/static/support.html'));
+app.get('/ita', (req, res) => res.sendFile(__dirname +'/static/ita.docx'));
 
 server.listen(port, () => {
     console.log(`PartieXpender is listening on ${port}`);
