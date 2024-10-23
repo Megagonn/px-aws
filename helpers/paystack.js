a0be176d3baeb21eac9adf22179e0e591914dff5
megagonn <yusuffrasheed.yr@gmail.com>
2024-10-23 13:04:05 +0100

    commit: oct 23

--- helpers/paystack.js
+++ helpers/paystack.js
@@ -1,56 +1,95 @@
 // const dedicated_account = 
 const axios = require('axios').default;
 const sk = process.env.SK
+const mak = process.env.MONIFY_API_KEY
+const msk = process.env.MONIFY_SECRET_KEY
+const contractCode = process.env.CONTRACT_CODE
+const rootURL = "https://sandbox.monnify.com";
+const reservedURL = "/api/v2/bank-transfer/reserved-accounts";
+const loginURL = "https://sandbox.monnify.com/api/v1/auth/login";
 
-const createDVAcustomer = async (customer, first_name, last_name, phone) => {
+//API KEY: MK_TEST_G82V12HHQU
+//SCRET KEY: SGVG0XN68VWXHFA4CSNPU87VXA8M7RWM
+
+const getTokenFromMonify = async()=>{
     try {
+        let encodedToken = Buffer.from(`${mak}:${msk}`).toString('base64');
+        console.log(encodedToken);
+        
+        let res = await axios.post(loginURL, {}, {
+            headers: {
+                "Authorization": `Basic ${encodedToken}`
+            }
+        })
+        console.log(res);
+        
+        if (res.data.requestSuccessful) {
+            return res.data.responseBody.accessToken;
+        } return false;
+
+    } catch (error) {
+        console.log(error);
+        return false;
+        
+    }
+}
+
+const createMonnifyAccount = async (customer, first_name, last_name, phone) => {
+    try {
+        let token = await getTokenFromMonify();
         const header = {
-            authorization: `Bearer ${sk}`,
+            authorization: `Bearer ${token}`,
             content_type: "Content-Type: application/json",
         }
-        let res = await axios.post("https://api.paystack.co/dedicated_account", {
-            customer,
-            "preferred_bank": "titan-paystack",
-            first_name,
-            last_name,
-            phone
-        }, {
+        let body = {
+            "accountReference": "my_ref1",
+            "accountName": "Adebowale JK",
+            "currencyCode": "NGN",
+            "contractCode": contractCode,
+            "customerEmail": "wolvedolph@gmail.com",
+            "bvn": "21212121212",
+            "customerName": "John Doe",
+            "getAllAvailableBanks": false,
+        };
+        let res = await axios.post(rootURL+reservedURL, body, {
             headers: header
         });
-        return res.data.status ? { status: true, payload: res.data } : { status: false, payload: res.data };
+        console.log(res.data);
+        
+        return res.data.requestSuccessful ? { status: true, payload: res.data } : { status: false, payload: res.data };
     } catch (error) {
         return { status: false, payload: error }
     }
 }
-const createAndAssignDVA = async (email, first_name, last_name, phone) => {
-    try {
-        const header = {
-            "Authorization": `Bearer ${sk}`,
-            "Content-Type": "application/json",
-        }
-        let createCustomer = await axios.post("https://api.paystack.co/customer", { email: email, first_name: first_name, last_name, phone: phone }, {
-            headers: header
-        });
-        console.log(createCustomer.data);
-        if (createCustomer.data.status) {
+// const createAndAssignDVA = async (email, first_name, last_name, phone) => {
+//     try {
+//         const header = {
+//             "Authorization": `Bearer ${sk}`,
+//             "Content-Type": "application/json",
+//         }
+//         let createCustomer = await axios.post("https://api.paystack.co/customer", { email: email, first_name: first_name, last_name, phone: phone }, {
+//             headers: header
+//         });
+//         console.log(createCustomer.data);
+//         if (createCustomer.data.status) {
 
-            let res = await axios.post("https://api.paystack.co/dedicated_account", {
-                customer: createCustomer.data.data.customer_code,
-                "preferred_bank": "titan-paystack",
-                email: email, first_name: first_name, last_name, phone: phone,
-                country: "NG"
-            }, {
-                headers: header
-            });
-            console.log(res.data);
-            return res.data.status ? { status: true, payload: res.data.data } : { status: false, payload: res.data.data };
-        } else {
+//             let res = await axios.post("https://api.paystack.co/dedicated_account", {
+//                 customer: createCustomer.data.data.customer_code,
+//                 "preferred_bank": "titan-paystack",
+//                 email: email, first_name: first_name, last_name, phone: phone,
+//                 country: "NG"
+//             }, {
+//                 headers: header
+//             });
+//             console.log(res.data);
+//             return res.data.status ? { status: true, payload: res.data.data } : { status: false, payload: res.data.data };
+//         } else {
 
-            return { status: false, payload: `DVA creation failed. \n\n${createCustomer.data.message}` }
-        }
-    } catch (error) {
-        return { status: false, payload: error }
-    }
-}
+//             return { status: false, payload: `DVA creation failed. \n\n${createCustomer.data.message}` }
+//         }
+//     } catch (error) {
+//         return { status: false, payload: error }
+//     }
+// }
 
-module.exports = { createDVAcustomer, createAndAssignDVA }
+module.exports = { createMonnifyAccount }
